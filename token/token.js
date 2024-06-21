01d6414e3a4648518af05f02e5200b527d2426fe
Ricardo <ricardoharanopinto@gmail.com>
2024-06-21 13:36:17 -0300

    implementação do token jwt

--- token/token.js
+++ token/token.js
@@ -1,2 +1,17 @@
 import jwt from "jsonwebtoken"
 
+const chaveSecreta = 'chaveSecreta'
+
+const token = jwt.sign(
+    {
+        apelido: "rica",
+        nome: "ricardo",
+        idade: "22"
+    }, chaveSecreta
+)
+
+console.log(token)
+
+const tokenDecodificado = jwt.verify(token,chaveSecreta)
+
+console.log(tokenDecodificado)
